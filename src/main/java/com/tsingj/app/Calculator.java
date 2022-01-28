0bc0097163514a6dfc0967377e1c86a0bb1860c8
limenglong <limenglong@tsingj.com>
2022-01-28 11:30:46 +0800

    Init commit

---
+++ src/main/java/com/tsingj/app/Calculator.java
@@ -0,0 +1,7 @@
+package com.tsingj.app;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/tsingj/steps/MathsStepDefinitions.java
@@ -0,0 +1,36 @@
+package com.tsingj.steps;
+
+import io.cucumber.java.en.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.tsingj.app.Calculator;
+
+public class MathsStepDefinitions {
+
+    int a = 0;
+    int b = 0;
+    int total = 0;
+
+    Calculator calculator = new Calculator();
+
+    @Given("a is {int}")
+    public void givenAIs(int value) {
+        a = value;
+    }
+
+    @Given("b is {int}")
+    public void givenBIs(int value) {
+        b = value;
+    }
+
+    @When("I add a and b")
+    public void iAddAAndB() {
+        total = calculator.add(a,b);
+    }
+
+    @Then("the total should be {int}")
+    public void theTotalShouldBe(int expectedTotal) {
+        assertThat(total).isEqualTo(expectedTotal);
+    }
+
+}
